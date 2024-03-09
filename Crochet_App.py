406635d687547402c9f3ad4c8b93a7290fa93be3
Aishcreates <162830381+Aishcreates@users.noreply.github.com>
2024-03-09 13:55:29 -0500

    Update Crochet_App.py

--- Crochet_App.py
+++ Crochet_App.py
@@ -3,7 +3,27 @@ import streamlit as st
 
 st.set_page_config(page_title="Homemade Sunshine", page_icon=":tada:", layout="wide")
 
+with st.container():
+    st.subheader("Welocme to Homemade sunshine! :wave:")
+    st.title("Homemade sunshine")
+    st.write("Me and my friends are passionate about makeing crochet items and selling them for profit ")
 
-st.subheader("Welocme to Homemade sunshine! :wave:")
-st.title("Homemade sunshine")
-st.write("Me and my friends are passionate about makeing crochet items and selling them for profit ")
+
+
+with st.container():
+    st.write("---")
+    left column, right column - st.columns(2)
+    with left_column:
+        st.header("What our team does")
+        st.write("##")
+        st.write(
+          " " "
+          On our website (designed by the buisness's co-owner) we sell items that were crocheted by hand as well as other crochet items that are very cheap compared to some original prices,
+          these crochet items are good for:
+          - Gifting (Options such as handbags, amugarami etc.)
+          - Your passion for crochet 
+          - Or just fashion in general!
+
+          If this sounds interesting to you, consider buying our products and supporing us!
+          " " "
+        
