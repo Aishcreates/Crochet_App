56baf2065f86d46c8be4f1b3690b84887583fb4d
Aishcreates <162830381+Aishcreates@users.noreply.github.com>
2024-03-09 14:31:23 -0500

    Update Crochet_App.py

--- Crochet_App.py
+++ Crochet_App.py
@@ -1,6 +1,6 @@
 import requests
 import streamlit as st
-from streamlit_lottie import st_lottie
+from streamlit-lottie import st_lottie
 
 
 st.set_page_config(page_title="Homemade Sunshine", page_icon=":tada:", layout="wide")
