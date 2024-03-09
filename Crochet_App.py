7a288e85627c677b7e7f8b850bd5070ec871aaf7
Aishcreates <162830381+Aishcreates@users.noreply.github.com>
2024-03-09 13:42:50 -0500

    Update Crochet_App.py

--- Crochet_App.py
+++ Crochet_App.py
@@ -1,1 +1,9 @@
 import streamlit as st
+
+
+st.set_page_config(page_title="Homemade Sunshine", page_icon=":tada:", layout="wide")
+
+
+st.subheader("Welocme to Homemade sunshine! :wave:")
+st.title("Homemade sunshine")
+st.write("Me and my friends are passionate about makeing crochet items and selling them for profit ")
